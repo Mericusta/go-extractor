578b841eabf57d46e74f05f17df908c67e7608dc
yunpeng.li <yunpeng.li@vaneplus.com>
2023-05-16 16:37:23 +0800

    #202305161637 add CleanFileComment

--- file.go
+++ file.go
@@ -6,9 +6,12 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 type GoFileMeta struct {
@@ -81,4 +84,64 @@ func GoFmtFile(p string) {
 	if err != nil {
 		panic(cmd.Stderr.(*bytes.Buffer).String())
 	}
+}
+
+// CleanFileComment 置空文件中所有注释
+func CleanFileComment(r io.Reader) string {
+	fileContent, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+
+	isBlock, isComment := false, false
+	firstCommentIndex, secondCommentIndex := -1, -1
+	builder, commentBuffer := strings.Builder{}, strings.Builder{}
+	for index, b := range fileContent {
+		switch rune(b) {
+		case PunctuationMarkLeftDoubleQuotes:
+			if !isComment {
+				if !isBlock {
+					isBlock = true
+				} else {
+					isBlock = false
+				}
+			}
+		case '/':
+			if !isBlock {
+				if firstCommentIndex == -1 {
+					firstCommentIndex = index
+				} else if secondCommentIndex == -1 {
+					secondCommentIndex = index
+					isComment = true
+					commentBuffer.Reset()
+				}
+			}
+		case '\n':
+			if isComment {
+				isComment = false
+				firstCommentIndex = -1
+				secondCommentIndex = -1
+				commentBuffer.Reset()
+			}
+		}
+
+		if !isComment {
+			if firstCommentIndex != -1 && secondCommentIndex == -1 {
+				if commentBuffer.Len() > 0 {
+					// just one /, clear comment buffer
+					builder.WriteString(commentBuffer.String())
+					builder.WriteByte(b)
+					firstCommentIndex = -1
+					commentBuffer.Reset()
+				} else {
+					// first match /
+					commentBuffer.WriteByte(b)
+				}
+			} else {
+				builder.WriteByte(b)
+			}
+		}
+	}
+
+	return builder.String()
 }
